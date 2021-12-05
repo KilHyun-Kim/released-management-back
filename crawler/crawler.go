ef6fec5462c813f448146edd3371453c15e293f2
KilHyun-Kim <rlaarlfgus@gmail.com>
2021-12-05 22:59:13 +0900

    Create crawler test Code useing go.colly

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -2,46 +2,90 @@ package crawler
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strings"
 
-	"github.com/antchfx/htmlquery"
+	"github.com/gocolly/colly"
+	"golang.org/x/net/html"
 )
 
-func getHtml() (string, error) {
-	url := "https://github.com/facebook/react/blob/main/CHANGELOG.md"
-	res, err := http.Get(url)
-
-	if err != nil {
-		fmt.Println(err)
-		return "", err
-	}
-
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-
-	if err != nil {
-		fmt.Println(err)
-		return "", err
-	}
-
-	return string(body), nil
+type List struct {
+	Title    string
+	Subtitle string
+	Contents []string
 }
 
-func Crawler() (string, error) {
-	html, err := getHtml()
-	fmt.Println(html)
-	if err != nil {
-		return html, err
+// func renderNode(n *html.Node) string {
+// 	var buf bytes.Buffer
+// 	w := io.Writer(&buf)
+// 	html.Render(w, n)
+// 	return buf.String()
+// }
+
+/*
+	Crawling Example useing Colly
+	http://go-colly.org/
+	for React Version page
+	https://github.com/facebook/react/blob/main/CHANGELOG.md
+*/
+func Crawler() {
+
+	c := colly.NewCollector(
+		// Visit only domains: github.com
+		colly.AllowedDomains("github.com"),
+	)
+	l := make([]List, 0)
+	var b List
+
+	// <ul> 태그 일 경우 > <li> 태그 안의 값들을 정리하는 함수
+
+	var organize func(*html.Node)
+	organize = func(n *html.Node) {
+
+		if n.Type == 1 && n.Data != "a" {
+			fmt.Printf(n.Data)
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			organize(c)
+		}
 	}
 
-	doc, err := htmlquery.Parse(strings.NewReader(html))
+	//e.Name : 해당 태그
+	//e.Text : 태그안의 값
 
-	list := htmlquery.Find(doc, "//a")
-	for _, val := range list {
-		fmt.Println("====google====", htmlquery.InnerText(val))
-	}
-	return html, err
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+		e.ForEach("article", func(_ int, el *colly.HTMLElement) {
+			for index, value := range el.DOM.Children().Nodes {
+				switch value.Data {
+				case "h2":
+					l = append(l, b)
+					b.Title = value.LastChild.Data
+					// fmt.Println("h2", value.LastChild.Data)
+				case "h3":
+					b.Subtitle = value.LastChild.Data
+					// fmt.Println("h3", value.LastChild.Data)
+				// case "h4":
+				// case "p":
+				case "ul":
+					fmt.Println(index)
+					organize(value)
+					// fmt.Println(value.LastChild.Data)
+				default:
+					// fallthrough
+					// fmt.Println("-")
+				}
+
+			}
+			fmt.Println(l)
+		})
+
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// Start scraping on https://hackerspaces.org
+	c.Visit("https://github.com/facebook/react/blob/main/CHANGELOG.md")
+
 }
--- server.go
+++ server.go
@@ -1,37 +1,30 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"kilhyun-kim/released-back/crawler"
-	"log"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 
-	go func() {
-		crawler.Crawler()
-	}()
+	// go func() {
+	// 	crawler.Crawler()
+	// }()
 
-	db, err := sql.Open("mysql", "root:pwd@tcp(127.0.0.1:3306)/gotest")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// db, err := sql.Open("mysql", "root:pwd@tcp(127.0.0.1:3306)/gotest")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	defer db.Close()
-	var name string
-	err = db.QueryRow("SELECT title FROM topic ").Scan(&name)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(name)
-	r := gin.Default()
-	r.POST("/api/test", func(c *gin.Context) {
-		c.JSON(http.StatusOK, name)
-	})
-	r.Run()
+	// defer db.Close()
+	// var name string
+	// err = db.QueryRow("SELECT title FROM topic ").Scan(&name)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(name)
+	// r := gin.Default()
+	// r.POST("/api/test", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, name)
+	// })
+	// r.Run()
 }
